42864a921c72a8c06c3072c0bdae5cd33c7b17be
Le Trung Hai <hozymary@gmail.com>
2022-03-05 21:50:57 +0800

    Implement scanning tokens
    Since Rust String is encoded in UTF-8, accessing the nth character
    (actually Unicode scalar value) is an O(n) operation. For now, I leave
    it as is, but this will need to be fixed.
    
    One option might be to use the `source.chars()` iterator directly,
    together with the method `next()` and `peek()`. Another is to store
    the `chars()` as a Vec inside the scanner. Either way, working with
    Unicode requires more care.

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,5 +1,5 @@
 use crate::token_type::TokenType::{self, *};
-use crate::token::*;
+use crate::{token::*, Lox};
 
 struct Scanner {
   source: String,
@@ -18,5 +18,52 @@ impl Scanner {
       current: 0,
       line: 1
     }
+  }
+
+  fn scan_tokens(&mut self) -> Vec<Token> {
+    while !(self.is_at_end()) {
+      self.start = self.current;
+      self.scan_token();
+    }
+  
+    let token = Token::new(EOF, "", None, self.line);
+    self.tokens.push(token);
+    self.tokens.clone()
+  }
+
+  fn scan_token(&mut self) {
+    let c: char = self.advance();
+    match c {
+      '(' => self.add_token(LEFT_PAREN, None),
+      ')' => self.add_token(RIGHT_PAREN, None),
+      '{' => self.add_token(LEFT_BRACE, None),
+      '}' => self.add_token(RIGHT_BRACE, None),
+      ',' => self.add_token(COMMA, None),
+      '.' => self.add_token(DOT, None),
+      '-' => self.add_token(MINUS, None),
+      '+' => self.add_token(PLUS, None),
+      ';' => self.add_token(SEMICOLON, None),
+      '*' => self.add_token(STAR, None),
+
+      // TODO: coalesce a run of invalid characters into a single error
+      _ => Lox::error(self.line, "Unexpected character"),
+    }
+  }
+
+  fn advance(&mut self) -> char {
+    // TODO: this is wildly inefficient, as it reiterate over the source
+    // string everytime. Maybe use the iterator directly?
+    self.current += 1;
+    self.source.chars().nth(self.current-1).unwrap()
+  }
+
+  fn add_token(&mut self, token_type: TokenType, literal: Option<Literal>) {
+    let text = self.source.get(self.start..self.current).unwrap();
+    let token = Token::new(token_type, text, literal, self.line);
+    self.tokens.push(token);
+  }
+
+  fn is_at_end(&self) -> bool {
+    self.current > self.source.len()
   }
 }
