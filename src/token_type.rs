ad8096164418843e6c626af96fcff8f83dbf6cfd
Le Trung Hai <hozymary@gmail.com>
2022-03-05 20:34:41 +0800

    Revert CamelCase due to clash with built-in String

--- src/token_type.rs
+++ src/token_type.rs
@@ -1,21 +1,21 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub(crate) enum TokenType {
   // Single-character tokens.
-  LeftParen, RightParen, LeftBrace, RightBrace,
-  Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
-  // One Or Two Character Tokens.
-  Bang, BangEqual,
-  Equal, EqualEqual,
-  Greater, GreaterEqual,
-  Less, LessEqual,
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
 
   // Literals.
-  Identifier, String, Number,
+  IDENTIFIER, STRING, NUMBER,
 
   // Keywords.
-  And, Class, Else, False, Fun, For, If, Nil, Or,
-  Print, Return, Super, This, True, Var, While,
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
 
-  Eof
+  EOF
 }
