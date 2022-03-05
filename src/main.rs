82d72a7a396356c765cfe66a30bfb3e92d916b79
Le Trung Hai <hozymary@gmail.com>
2022-03-05 17:52:54 +0800

    OOP baby

--- src/main.rs
+++ src/main.rs
@@ -7,52 +7,63 @@ use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let mut program = Lox::new();
     if args.len() > 2 {
         println!("Usage: rlox [script]");
         process::exit(1);
     } else if args.len() == 2 {
-        run_file(&args[2]);
+        program.run_file(&args[2]);
     } else {
-        run_prompt();
+        program.run_prompt();
     }
 }
 
-fn run_file(path: &str) -> Result<(), Box<dyn Error>> {
-    let content: String = fs::read_to_string(path)?;
-    run(&content);
-    Ok(())
+struct Lox {
+    had_error: bool,
 }
 
-fn run_prompt() {
-    loop {
-        print!("> ");
-        io::stdout().flush().expect("flush failed!");
+impl Lox {
+    fn new() -> Self {
+        Self { had_error: false }
+    }
 
-        let mut buffer = String::new();
-        match io::stdin().read_line(&mut buffer) {
-            Ok(n) => {
-                if n == 0 {
+    fn run_file(&mut self, path: &str) -> Result<(), Box<dyn Error>> {
+        let content: String = fs::read_to_string(path)?;
+        self.run(&content);
+        Ok(())
+    }
+
+    fn run_prompt(&mut self) {
+        loop {
+            print!("> ");
+            io::stdout().flush().expect("flush failed!");
+
+            let mut buffer = String::new();
+            match io::stdin().read_line(&mut buffer) {
+                Ok(n) => {
+                    if n == 0 {
+                        break;
+                    } else {
+                        self.run(&buffer);
+                    }
+                },
+                Err(error) => {
+                    println!("error: {error}");
                     break;
-                } else {
-                    run(&buffer);
-                }
-            },
-            Err(error) => {
-                println!("error: {error}");
-                break;
-            },
+                },
+            }
         }
     }
-}
 
-fn run(source: &str) {
-    print!("{}", source);
-}
+    fn run(&mut self, source: &str) {
+        print!("{}", source);
+    }
 
-fn error(line: usize, message: &str) {
-    report(line, "", message);
-}
+    fn error(line: usize, message: &str) {
+        Self::report(line, "", message);
+    }
 
-fn report(line: usize, err: &str, message: &str) {
-    println!("[line {line}] Error {err}: {message}");
+    fn report(line: usize, err: &str, message: &str) {
+        println!("[line {line}] Error {err}: {message}");
+    }
 }
