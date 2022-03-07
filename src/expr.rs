3fe9be76d7da656ec1a735c4ed654f99d672aca9
Le Trung Hai <hozymary@gmail.com>
2022-03-07 20:21:54 +0800

    Add Expr for syntax tree
    Add enum Expr containing the expressions in the grammar, each of which
    is boxed, as this is a recursive type. Each type of expression is a
    struct. I think a trait would be much cleaner; I will try in another
    branch later.
    
    I decided to stray from the visitor pattern used in the book, since I
    think matching an enum is not that bad. However, this might change.
    
    At first, I attempted to generate the Expr using declarative macros,
    but alas that is a terrible way to write code, as you could not even
    see the structs you are using. I probably should use proc macros, but
    for now, copy and paste is good enough.
    
    (I did pull in a whole crate to derive the `new` constructor, though)

---
+++ src/expr.rs
@@ -0,0 +1,57 @@
+use crate::token::{LiteralToken, Token};
+use derive_new::new;
+
+#[derive(Debug)]
+pub(crate) enum Expr {
+  Binary(Box<Binary>),
+  Grouping(Box<Grouping>),
+  Literal(Box<Literal>),
+  Unary(Box<Unary>),
+}
+
+#[derive(new, Debug)]
+pub(crate) struct Binary {
+  pub(crate) left: Expr,
+  pub(crate) operator: Token,
+  pub(crate) right: Expr,
+}
+
+#[derive(new, Debug)]
+pub(crate) struct Grouping {
+  pub(crate) expression: Expr,
+}
+
+#[derive(new, Debug)]
+pub(crate) struct Literal {
+  pub(crate) value: Option<LiteralToken>,
+}
+
+#[derive(new, Debug)]
+pub(crate) struct Unary {
+  pub(crate) operator: Token,
+  pub(crate) right: Expr,
+}
+
+impl From<Binary> for Expr {
+    fn from(b: Binary) -> Self {
+        Self::Binary(Box::new(b))
+    }
+}
+
+impl From<Grouping> for Expr {
+    fn from(b: Grouping) -> Self {
+        Self::Grouping(Box::new(b))
+    }
+}
+
+impl From<Literal> for Expr {
+    fn from(b: Literal) -> Self {
+        Self::Literal(Box::new(b))
+    }
+}
+
+impl From<Unary> for Expr {
+    fn from(b: Unary) -> Self {
+        Self::Unary(Box::new(b))
+    }
+}
