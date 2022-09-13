339ff4f6e1cf59f3d13d9dd0896806260011f826
Le Trung Hai <hozymary@gmail.com>
2022-09-13 20:26:34 +0800

    Change project name to primox

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::env;
 use std::process;
 
-use rlox::Lox;
+use primox::Lox;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut program = Lox::new();
     if args.len() > 2 {
-        println!("Usage: rlox [script]");
+        println!("Usage: primox [script]");
         process::exit(1);
     } else if args.len() == 2 {
         program.run_file(&args[1]);
